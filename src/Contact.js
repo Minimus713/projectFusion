429c2f33c0f611f2447b95f309ed6c0adba56ff1
J-Doosk <jonjongough@yahoo.com>
2020-04-19 22:06:35 -0700

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>GOT MILK?</h2>
+                <p>The easiest thing to do in a bad situation is look here</p>
+                <a href={"http://www.cutestpaw.com/tag/cats/"}>Yup</a>
+            </div>
+        );
+    }
+}
+export default Contact;
---
+++ src/Stuff.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Stuff extends Component {
+    render() {
+        return (
+            <div>
+                <h2>STUFF</h2>
+                <p>The opposite of a giraffe is a seal</p>
+                <ol>
+                    <li>Change my mind</li>
+                    <li>Darth Plagueis</li>
+                    <li>Bing Bong</li>
+                    <li>Elon Musk</li>
+                </ol>
+            </div>
+        );
+    }
+}
+export default Stuff;
